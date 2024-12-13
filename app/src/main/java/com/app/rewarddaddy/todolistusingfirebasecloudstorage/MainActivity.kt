40180ade6a8d6afbaf61049c4d7f2ec34bcfd681
vikassharma2899 <152948138+vikassharma2899@users.noreply.github.com>
2024-12-13 21:46:22 +0530

    Creating Ui

---
+++ app/src/main/java/com/app/rewarddaddy/todolistusingfirebasecloudstorage/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.app.rewarddaddy.todolistusingfirebasecloudstorage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.rewarddaddy.todolistusingfirebasecloudstorage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    binding=ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+
+    }
+}
